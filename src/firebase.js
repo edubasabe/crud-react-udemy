8b8c7d7c5acdd86babbef15938f1e821fe641905
David <david.basabe@cencosud.cl>
2021-02-21 11:30:02 -0300

    :sparkles: Adding prettier rules to package.json and firebase api key to env

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,15 @@ import app from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
 const firebaseConfig = {
-  apiKey: "AIzaSyDHHBke4MHEAhYm-qW4Au05IZsKzypHJj4",
-  authDomain: "crud-react-b797d.firebaseapp.com",
-  databaseURL: "https://crud-react-b797d.firebaseio.com",
-  projectId: "crud-react-b797d",
-  storageBucket: "crud-react-b797d.appspot.com",
-  messagingSenderId: "412476770665",
-  appId: "1:412476770665:web:ccfbe7f88db7a7905bd5f8"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: 'crud-react-b797d.firebaseapp.com',
+  databaseURL: 'https://crud-react-b797d.firebaseio.com',
+  projectId: 'crud-react-b797d',
+  storageBucket: 'crud-react-b797d.appspot.com',
+  messagingSenderId: '412476770665',
+  appId: '1:412476770665:web:ccfbe7f88db7a7905bd5f8',
 };
 
 // Initialize Firebase
